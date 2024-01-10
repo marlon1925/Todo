1c36ce4f4e31c875f931a819682b5ab56a52a8dd
WilsonG08 <wilson.guayanay@epn.edu.ec>
2024-01-10 18:02:59 -0500

    Agregar archivos de proyecto.

---
+++ Todo/Todo/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Todo
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            this.BackgroundColor = Color.FromHex("#ADD8E6"); 
+        }
+
+    }
+}
